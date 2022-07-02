f33b456b5a109ac3671e3dbd7b752ddbe88a2ec9
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-07-03 00:35:42 +0300

    added

--- src/components/Bundles.js
+++ src/components/Bundles.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// import icons
+import { BsCheckCircleFill } from 'react-icons/bs';
+
 const Bundle = ({ bundles }) => {
   return (
     <div className='flex flex-col lg:flex-row justify-between gap-[30px]'>
@@ -9,7 +12,7 @@ const Bundle = ({ bundles }) => {
         const { name, price, list } = bundle;
         return (
           <div
-            className='w-full max-w-1/3 bg-white shadow-primary text-center h-[560px] rounded-[60px] py-12'
+            className='w-full max-w-1/3 bg-white shadow-primary text-center h-[560px] rounded-[60px] py-12 px-8'
             key={index}
           >
             <div className='text-5xl font-semibold text-blue mb-4'>
@@ -18,7 +21,12 @@ const Bundle = ({ bundles }) => {
             <div className='capitalize'>{name}</div>
             <div>
               {list.map((item, index) => {
-                return <div key={index}>{item}</div>;
+                return (
+                  <div className='flex items-center gap-x-3' key={index}>
+                    <BsCheckCircleFill className='text-orange' />
+                    <div className='capitalize'>{item}</div>
+                  </div>
+                );
               })}
             </div>
           </div>
